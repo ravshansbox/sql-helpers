40c6d5b065efe6bbeae230e635241e2e05b72c07
Ravshan Samandarov <ravshansbox@gmail.com>
2019-08-01 14:25:56 +0500

    fix test for flatten

--- index.spec.ts
+++ index.spec.ts
@@ -7,6 +7,6 @@ describe('flatten', () => {
   });
 
   it('should flatten an array', () => {
-    expect(flatten([1, 2, [3, 4, [5, 6], 7, 8], 9, 10])).not.toBe([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(flatten([1, 2, [3, 4, [5, 6], 7, 8], 9, 10])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 });
